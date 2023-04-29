6ee788810342b3fc0188542ac290614167550a27
phamhungd <phamhung.d@gmail.com>
2023-04-30 01:37:34 +0700

    Update source

--- modules/ui_extra_networks_checkpoints.py
+++ modules/ui_extra_networks_checkpoints.py
@@ -1,6 +1,7 @@
 import html
 import json
 import os
+import urllib.parse
 
 from modules import shared, ui_extra_networks, sd_models
 
@@ -16,14 +17,21 @@ class ExtraNetworksPageCheckpoints(ui_extra_networks.ExtraNetworksPage):
         checkpoint: sd_models.CheckpointInfo
         for name, checkpoint in sd_models.checkpoints_list.items():
             path, ext = os.path.splitext(checkpoint.filename)
+            previews = [path + ".png", path + ".preview.png"]
+
+            preview = None
+            for file in previews:
+                if os.path.isfile(file):
+                    preview = self.link_preview(file)
+                    break
+
             yield {
                 "name": checkpoint.name_for_extra,
                 "filename": path,
-                "preview": self.find_preview(path),
-                "description": self.find_description(path),
+                "preview": preview,
                 "search_term": self.search_terms_from_path(checkpoint.filename) + " " + (checkpoint.sha256 or ""),
                 "onclick": '"' + html.escape(f"""return selectCheckpoint({json.dumps(name)})""") + '"',
-                "local_preview": f"{path}.{shared.opts.samples_format}",
+                "local_preview": path + ".png",
             }
 
     def allowed_directories_for_previews(self):
--- test/server_poll.py
+++ test/server_poll.py
@@ -1,8 +1,6 @@
 import unittest
 import requests
 import time
-import os
-from modules.paths import script_path
 
 
 def run_tests(proc, test_dir):
@@ -17,8 +15,8 @@ def run_tests(proc, test_dir):
                 break
     if proc.poll() is None:
         if test_dir is None:
-            test_dir = os.path.join(script_path, "test")
-        suite = unittest.TestLoader().discover(test_dir, pattern="*_test.py", top_level_dir=test_dir)
+            test_dir = "test"
+        suite = unittest.TestLoader().discover(test_dir, pattern="*_test.py", top_level_dir="test")
         result = unittest.TextTestRunner(verbosity=2).run(suite)
         return len(result.failures) + len(result.errors)
     else:
